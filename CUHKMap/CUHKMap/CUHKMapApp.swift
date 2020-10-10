19fb412a4c15582efc5e746ddb9e2811d99002da
wanruuu <1155107790@link.cuhk.edu.hk>
2020-10-10 22:25:44 +0800

    add project file

---
+++ CUHKMap/CUHKMap/CUHKMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CUHKMapApp.swift
+//  CUHKMap
+//
+//  Created by wanruuu on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CUHKMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
