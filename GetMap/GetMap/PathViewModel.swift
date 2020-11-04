e813c186de1647ae4c074e869f95d0977084adba
wanruuu <1155107790@link.cuhk.edu.hk>
2020-11-04 21:56:44 +0800

    fix bug of zoom in/out

--- GetMap/GetMap/PathViewModel.swift
+++ GetMap/GetMap/PathViewModel.swift
@@ -23,11 +23,13 @@ struct StraightPath: View {
                 print(pathUnit.end_point)
                 return }
             let p1 = CGPoint(
-                x: centerX + CGFloat((pathUnit.start_point[1] - locationGetter.current.coordinate.longitude)*85390*2) + offset.x,
-                y: centerY + CGFloat((locationGetter.current.coordinate.latitude - pathUnit.start_point[0])*111000*2) + offset.y)
+                x: centerX + CGFloat((pathUnit.start_point[1] - locationGetter.current.coordinate.longitude)*85390*2) * scale + offset.x,
+                y: centerY + CGFloat((locationGetter.current.coordinate.latitude - pathUnit.start_point[0])*111000*2) * scale + offset.y
+            )
             let p2 = CGPoint(
-                x: centerX + CGFloat((pathUnit.end_point[1] - locationGetter.current.coordinate.longitude)*85390*2) + offset.x,
-                y: centerY + CGFloat((locationGetter.current.coordinate.latitude - pathUnit.end_point[0])*111000*2) + offset.y)
+                x: centerX + CGFloat((pathUnit.end_point[1] - locationGetter.current.coordinate.longitude)*85390*2) * scale + offset.x,
+                y: centerY + CGFloat((locationGetter.current.coordinate.latitude - pathUnit.end_point[0])*111000*2) * scale + offset.y
+            )
             path.move(to: p1)
             path.addLine(to: p2)
         }.stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineJoin: .round))
