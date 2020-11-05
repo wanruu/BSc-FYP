5d2ca2fd4bc21ca9f19075138cf6963ddd32e4e8
wanruuu <1155107790@link.cuhk.edu.hk>
2020-11-05 15:40:19 +0800

    delete struct Vector, add one more * func in Point, also change the parameter name

--- GetMap/GetMap/Functions.swift
+++ GetMap/GetMap/Functions.swift
@@ -19,11 +19,6 @@ struct Point {
     var y: Double
     var z: Double
 }
-struct Vector {
-    var x: Double
-    var y: Double
-    var z: Double
-}
 extension Point {
     static func + (left: Point, right: Point) -> Point {
         return Point(x: left.x + right.x, y: left.y + right.y, z: left.z + right.z)
@@ -34,8 +29,11 @@ extension Point {
     static func * (left: Point, right: Point) -> Double {
         return left.x * right.x + left.y * right.y + left.z * right.z
     }
-    static func * (left: Double, right: Point) -> Point {
-        return Point(x: right.x * left, y: right.y * left, z: right.z * left)
+    static func * (para: Double, p: Point) -> Point {
+        return Point(x: p.x * para, y: p.y * para, z: p.z * para)
+    }
+    static func * (p: Point, para: Double)  -> Point {
+        return Point(x: p.x * para, y: p.y * para, z: p.z * para)
     }
 }
 
