b4ab8be6e54eddbb786e41d6c946473777a15245
wanruuu <1155107790@link.cuhk.edu.hk>
2020-11-09 01:27:24 +0800

    restore to last version

--- GetMap/GetMap/DataStructures/PathUnit.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PathUnit.swift
-//  GetMap
-//
-//  Created by wanruuu on 9/11/2020.
-//
-
-import Foundation
---
+++ GetMap/PathUnit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PathUnit+CoreDataClass.swift
+//  GetMap
+//
+//  Created by wanruuu on 29/10/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PathUnit)
+public class PathUnit: NSManagedObject {
+
+}
