6d8b0b80fd7edb24136996e49ffa969775de4206
gazerchen <62714185+gazerchen@users.noreply.github.com>
2020-12-02 00:38:57 +0800

    Add files via upload
    Home Page UI Change

--- GetMap/GetMap/Page/MainPage.swift
+++ GetMap/GetMap/Page/MainPage.swift
@@ -13,16 +13,54 @@ struct MainPage: View {
     
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: MapPage(locations: $locations, trajectories: $trajectories, lineSegments: $lineSegments, representatives: $representatives, locationGetter: locationGetter)) {
-                    Text("Map").bold()
+            
+              ScrollView (.horizontal, showsIndicators: false) {
+                HStack (spacing: 10) {
+                    NavigationLink(destination: MapPage(locations: $locations, trajectories: $trajectories, lineSegments: $lineSegments, representatives: $representatives, locationGetter: locationGetter)) {
+                        ZStack {
+                            Image("map")
+                                .resizable()
+                                .frame(width: 250, height: 250)
+                                .cornerRadius(20)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Map")
+                                    .foregroundColor(Color.white) .shadow(color: Color.black, radius: 3, x: 3, y: 3)
+                                    .font(.system(size: 60, weight: .bold, design: .rounded))
+                                 
+                            }.offset(x: -30, y: 80)
+                            
+                        }.frame(alignment: .center) .padding(.leading, 30)
+                    }
+                
+                    NavigationLink(destination: LocationPage(locations: $locations)) {
+                        ZStack {
+                            Image("building")
+                                .resizable()
+                                .frame(width: 250, height: 250)
+                                .cornerRadius(20)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Building")
+                                    .foregroundColor(Color.white).shadow(color: Color.black, radius: 3, x: 3, y: 3)
+                                    .font(.system(size: 55, weight: .bold, design: .rounded))
+                                   
+                            }
+                            .offset(x: -10, y: 80)
+                        }.frame(alignment: .center).padding(.trailing, 30)
+           
+
                 }
-                NavigationLink(destination: LocationPage(locations: $locations)) {
-                    Text("Location List").bold()
-                }
-            }
+                .padding(.leading, 30)
+             }
+              .padding(.top, 20)
+            
+              
                 .navigationTitle("Home")
-                .navigationBarTitleDisplayMode(.inline)
+
         }
-    }
+             
+    } 
+  
+}
 }
