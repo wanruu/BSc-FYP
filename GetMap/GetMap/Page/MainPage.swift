6b312a9ed1baed9f21962acffd087fefbe2c2b7e
wanruuu <1155107790@link.cuhk.edu.hk>
2020-12-06 01:07:46 +0800

    polish ui

--- GetMap/GetMap/Page/MainPage.swift
+++ GetMap/GetMap/Page/MainPage.swift
@@ -13,46 +13,38 @@ struct MainPage: View {
     
     @ObservedObject var locationGetter = LocationGetterModel()
     
+    // 1 = SCWidth * 0.001
     var body: some View {
         NavigationView {
-            ScrollView (.horizontal, showsIndicators: false) {
-                HStack (spacing: 10) {
-                    NavigationLink(destination: MapPage(locations: $locations, trajectories: $trajectories, lineSegments: $lineSegments, representatives: $representatives, p: $p, mapSys: $mapSys, locationGetter: locationGetter)) {
-                        ZStack {
-                            Image("map")
-                                .resizable()
-                                .frame(width: SCWidth * 0.25, height: SCWidth * 0.25) // 1 = SCWidth * 0.001
-                                .cornerRadius(SCWidth * 0.02)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Map")
-                                    .foregroundColor(Color.white) .shadow(color: Color.black, radius: SCWidth * 0.003, x: SCWidth * 0.003, y: SCWidth * 0.003)
-                                    .font(.system(size: SCWidth * 0.06, weight: .bold, design: .rounded))
-                                 
-                            }.offset(x: -SCWidth * 0.03, y: SCWidth * 0.08)
-                            
-                        }.frame(alignment: .center) .padding(.leading, SCWidth * 0.03)
+            HStack {
+                NavigationLink(destination: MapPage(locations: $locations, trajectories: $trajectories, lineSegments: $lineSegments, representatives: $representatives, p: $p, mapSys: $mapSys, locationGetter: locationGetter)) {
+                    ZStack {
+                        Image("map")
+                            .resizable()
+                            .frame(width: SCWidth * 0.25, height: SCWidth * 0.25)
+                            .cornerRadius(SCWidth * 0.05)
+                        Text("Map")
+                            .foregroundColor(Color.white) .shadow(color: Color.black, radius: SCWidth * 0.003, x: SCWidth * 0.003, y: SCWidth * 0.003)
+                            .font(.system(size: SCWidth * 0.06, weight: .bold, design: .rounded))
+                            .offset(x: 0, y: SCWidth * 0.06)
                     }
-                
-                    NavigationLink(destination: LocationPage(locations: $locations)) {
-                        ZStack {
-                            Image("building")
-                                .resizable()
-                                .frame(width: SCWidth * 0.25, height: SCWidth * 0.25)
-                                .cornerRadius(SCWidth * 0.02)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Location")
-                                    .foregroundColor(Color.white).shadow(color: Color.black, radius: SCWidth * 0.003, x: SCWidth * 0.003, y: SCWidth * 0.003)
-                                    .font(.system(size: SCWidth * 0.055, weight: .bold, design: .rounded))
-                                   
-                            }.offset(x: -SCWidth * 0.01, y: SCWidth * 0.08)
-                        }.frame(alignment: .center).padding(.trailing, SCWidth * 0.03)
-                    }.padding(.leading, SCWidth * 0.03)
-                }
-                .padding(.top, SCWidth * 0.02)
-                .navigationTitle("Home")
+                }.padding()
+                NavigationLink(destination: LocationPage(locations: $locations)) {
+                    ZStack {
+                        Image("building")
+                            .resizable()
+                            .frame(width: SCWidth * 0.25, height: SCWidth * 0.25)
+                            .cornerRadius(SCWidth * 0.05)
+                        
+                        Text("Location")
+                            .foregroundColor(Color.white).shadow(color: Color.black, radius: SCWidth * 0.003, x: SCWidth * 0.003, y: SCWidth * 0.003)
+                            .font(.system(size: SCWidth * 0.055, weight: .bold, design: .rounded))
+                            .offset(x: 0, y: SCWidth * 0.06)
+                    }
+                }.padding()
             }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
