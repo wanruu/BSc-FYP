ba93b8bf8ae31043ac621c9d3af8ea8d722217cb
wanruuu <1155107790@link.cuhk.edu.hk>
2020-12-07 20:17:09 +0800

    backend code

---
+++ backend/back.js
@@ -0,0 +1,294 @@
+/* require modules */
+var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var http = require('http');
+var cors = require('cors');
+
+/* define app to use express */
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}), bodyParser.json());
+
+/* session */
+app.use(session({
+    secret: 'CUMap',
+    cookie: { maxAge: 1000*60*60 }
+}));
+
+/* cors */
+app.use(cors());
+
+/* connect to mongodb */
+var mongodb = 'mongodb://localhost:27017/CUMap';
+mongoose.set('useCreateIndex', true);
+mongoose.connect(mongodb, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connection is open...');
+});
+
+/* define schema */
+var Schema = mongoose.Schema;
+
+var LocationSchema = Schema({
+    name_en: { type: String, require: true },
+    latitude: { type: Number, require: true },
+    longitude: { type: Number, require: true },
+    altitude: { type: Number, require: true },
+    type: { type: Number, require: true }
+});
+var TrajectorySchema = Schema({
+    points: [{ latitude: Number, longitude: Number, altitude: Number }],
+    timestamp: { type: Date }
+});
+var PathSchema = Schema({
+    start: { type: Schema.Types.ObjectId, ref: 'Location' },
+    end: { type: Schema.Types.ObjectId, ref: 'Location' },
+    path: [{ latitude: Number, longitude: Number, altitude: Number }],
+    dist: { type: Number, require: true },
+    type: { type: Number, require: true } // 0: on foot, 1: by bus
+});
+var VersionSchema = Schema({
+    database: { type: String, require: true, unique: true},
+    version: { type: String, require: true }
+})
+
+/* define model */
+const LocationModel = mongoose.model('Location', LocationSchema);
+const TrajectoryModel = mongoose.model('Trajectory', TrajectorySchema);
+const PathModel = mongoose.model('Path', PathSchema);
+const VersionModel = mongoose.model('Version', VersionSchema);
+
+/* set header */
+/* app.all('/', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}); */
+
+/* MARK: - get map img */
+/* app.get('/map', (req, res) => {
+    console.log("get");
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.sendFile("cuhk-campus-map.jpg", {root: __dirname});
+}); */
+
+/* admin login */
+// TODO
+
+app.get('/versions', (req, res) => {
+    console.log("GET /versions - " + Date());
+    VersionModel.find({}, (err, result) => {
+        if(err) {
+            res.send({operation: "get", target: "version", success: false, data: []});
+        } else {
+            res.send({operation: "get", target: "version", success: true, data: result})
+        }
+    });
+});
+
+app.put('/version', (req, res) => {
+    console.log("PUT /version - " + Date());
+    var database = req.body.database
+    var version = req.body.version
+    VersionModel.findOne({database: database}, (err, result) => {
+        if(err) {
+            res.send({operation: "unknown", target: "version", success: false, data: []});
+        } else if(result) {
+            VersionModel.updateOne({database: database}, {$set: {version: version}}, (err, result) => {
+                if(err) {
+                    res.send({operation: "update", target: "version", success: false, data: []});
+                } else {
+                    res.send({operation: "update", target: "version", success: true, data: []});
+                }
+            });
+        } else {
+            VersionModel.create({database: database, version: version}, (err, result) => {
+                if(err) {
+                    res.send({operation: "add", target: "version", success: false, data: []});
+                } else {
+                    res.send({operation: "add", target: "version", success: true, data: []});
+                }
+            });
+        }
+    });
+});
+
+/* MARK: - Location Model */
+app.put('/location', (req, res) => {
+    console.log("PUT /location - " + Date());
+    var name_en = req.body.name_en;
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    var altitude = req.body.altitude;
+    var type = req.body.type;
+    LocationModel.findOne({name_en: name_en, type: type}, (err, result) => {
+        if(err) {
+            res.send({operation: "unknown", target: "location", success: false, data: []});
+        } else if(result) {
+            var conditions = {name_en: name_en, type: type};
+            var update = {$set: {latitude: latitude, longitude: longitude, altitude: altitude}};
+            LocationModel.updateOne(conditions, update, (err, result) => {
+                if(err) {
+                    res.send({operation: "update", target: name_en, success: false, data: []});
+                }
+                else {
+                    res.send({operation: "update", target: name_en, success: true, data: []});
+                }
+            });
+        } else {
+            var newLocation = {name_en: name_en, latitude: latitude, longitude: longitude, altitude: altitude, type: type};
+            LocationModel.create(newLocation, (err, result) => {
+                if(err) {
+                    res.send({operation: "add", target: name_en, success: false, data: []});
+                }
+                else {
+                    res.send({operation: "add", target: name_en, success: true, data: []});
+                }
+            });
+        }
+    });
+});
+
+app.get('/locations', (req, res) => {
+    console.log("GET /locations - " + Date());
+    res.setHeader('Content-Type', 'application/json');
+    LocationModel.find({}, (err, result) => {
+        if(err) {
+            res.send({operation: "get", target: "locations", success: false, data: []});
+        } else {
+            res.send({operation: "get", target: "locations", success: true, data: result});
+        }
+    })
+});
+
+app.delete('/location', (req, res) => {
+    console.log("DELETE /location - " + Date());
+    var name_en = req.body.name_en;
+    var type = req.body.type;
+    var conditions = { name_en: name_en, type: type }
+    LocationModel.deleteOne(conditions, (err, result) => {
+        if(err) {
+            res.send({operation: "delete", targe: name_en, success: false, data: []});
+        } else {
+            res.send({operation: "delete", target: name_en, success: true, data: []});
+        }
+    });
+});
+
+/* MARK: - Trajectory Model */
+app.post('/trajectory', (req, res) => {
+    console.log("POST /trajectory - " + Date())
+    var points = req.body.points;
+    var time = Date();
+    TrajectoryModel.create({points: points, timestamp: time}, (err, result) => {
+        if(err) {
+            res.send({operation: "add", target: "trajectory", success: false, data: []});
+        } else {
+            res.send({operation: "add", target: "trajectory", success: true, data: []});
+        }
+    });
+});
+app.get('/trajectories', (req, res) => {
+    console.log("GET /trajectories - " + Date());
+    TrajectoryModel.find({}, (err, result) => {
+        if(err) {
+            res.send({operation: "get", target: "trajectories", success: false, data: []});
+        } else {            
+            var trajs = []
+            for(var i in result) {
+                trajs.push(result[i].points)
+            }
+            res.send({operation: "get", target: "trajectories", success: true, data: trajs});
+        }
+    })
+});
+app.delete('/trajectory', (req, res) => {
+    console.log("DELETE /trajectory - " + Date());
+    var ObjectId = require('mongodb').ObjectID
+    TrajectoryModel.deleteOne({_id: ObjectId(req.body.id)}, (err, result) => {
+        if(err) {
+            res.send({operation: "delete", target: "trajectory", success: false, data: []});
+        } else {
+            res.send({operation: "delete", target: "trajectory", success: true, data: []});
+        }
+    });
+});
+
+// proven to unable to deal with large input
+/* app.post('/paths', (req, res) => {
+    console.log("POST /paths - " + Date());
+    PathModel.deleteMany({}, (err, result) => {
+        if(err) {
+            res.send({operation: "add", target: "paths", success: false, data: []});
+        } else {
+            var items = req.body.data;
+            for(var i in items) {
+                var item = items[i];
+                LocationModel.findOne({name_en: item.start.name_en, type: item.start.type}, (err, result1) => {
+                    if(result1) {
+                        LocationModel.findOne({name_en: item.end.name_en, type: item.end.type}, (err, result2) => {
+                            if(result2) {
+                                var conditions = {start: result1._id, end: result2._id, path: item.path, dist: item.dist, type: item.type}
+                                PathModel.create(conditions, (err, result) => {
+                                    if(err) {
+                                        res.send({operation: "add", target: "paths", success: false, data: []});
+                                    } else {
+                                        res.send({operation: "add", target: "paths", success: true, data: []});
+                                    }
+                                });
+                            } else {
+                                res.send({operation: "add", target: "paths", success: false, data: []});
+                            }
+                        });
+                    } else {
+                        res.send({operation: "add", target: "paths", success: false, data: []});
+                    }
+                });
+            }
+        }
+    });
+});*/
+
+app.post('/path', (req, res) => {
+    console.log("POST /path - " + Date());
+    var data = req.body.data;
+    LocationModel.findOne({name_en: data.start.name_en, type: data.start.type}, (err, result1) => {
+        if(result1) {
+            LocationModel.findOne({name_en: data.end.name_en, type: data.end.type}, (err, result2) => {
+                if(result2) {
+                    var conditions = {start: result1._id, end: result2._id, path: data.path, dist: data.dist, type: data.type}
+                    PathModel.create(conditions, (err, result) => {
+                        if(err) {
+                            res.send({operation: "add", target: "path", success: false, data: []});
+                        } else {
+                            res.send({operation: "add", target: "path", success: true, data: []});
+                        }
+                    });
+                } else {
+                    res.send({operation: "add", target: "path", success: false, data: []});
+                }
+            });
+        } else {
+            res.send({operation: "add", target: "path", success: false, data: []});
+        }
+    });
+});
+
+app.get('/paths', (req, res) => {
+    console.log("GET /paths - " + Date());
+    PathModel.find({}).populate([{path: 'start', select: 'name_en type -_id'}, {path: 'end', select: 'name_en type -_id'}]).exec((err, result) => {
+        if(err) {
+            res.send({operation: "get", target: "paths", success: false, data: []})
+        } else {
+            res.send({operation: "get", target: "paths", success: true, data: result})
+        }
+    });
+});
+
+http.createServer(app).listen(8000);
+
