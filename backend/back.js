bf81dc44da2f9f41437e517b6d27a3e832fa990a
wanruuu <1155107790@link.cuhk.edu.hk>
2020-12-22 19:17:59 +0800

    Change structure sent by backend

--- backend/back.js
+++ backend/back.js
@@ -43,10 +43,10 @@ var TrajectorySchema = Schema({
     points: [{ latitude: Number, longitude: Number, altitude: Number }],
     timestamp: { type: Date }
 });
-var PathSchema = Schema({
-    start: { type: Schema.Types.ObjectId, ref: 'Location' },
-    end: { type: Schema.Types.ObjectId, ref: 'Location' },
-    path: [{ latitude: Number, longitude: Number, altitude: Number }],
+var RouteSchema = Schema({
+    startId: { type: Schema.Types.ObjectId, ref: 'Location' },
+    endId: { type: Schema.Types.ObjectId, ref: 'Location' },
+    points: [{ latitude: Number, longitude: Number, altitude: Number }],
     dist: { type: Number, require: true },
     type: { type: Number, require: true } // 0: on foot, 1: by bus
 });
@@ -58,7 +58,7 @@ var VersionSchema = Schema({
 /* define model */
 const LocationModel = mongoose.model('Location', LocationSchema);
 const TrajectoryModel = mongoose.model('Trajectory', TrajectorySchema);
-const PathModel = mongoose.model('Path', PathSchema);
+const RouteModel = mongoose.model('Route', RouteSchema);
 const VersionModel = mongoose.model('Version', VersionSchema);
 
 /* set header */
@@ -84,9 +84,10 @@ app.get('/versions', (req, res) => {
     console.log("GET /versions - " + Date());
     VersionModel.find({}, (err, result) => {
         if(err) {
-            res.send({operation: "get", target: "version", success: false, data: []});
+            console.log(err);
+            res.status(404).send();
         } else {
-            res.send({operation: "get", target: "version", success: true, data: result})
+            res.send(result)
         }
     });
 });
@@ -97,21 +98,24 @@ app.put('/version', (req, res) => {
     var version = req.body.version
     VersionModel.findOne({database: database}, (err, result) => {
         if(err) {
-            res.send({operation: "unknown", target: "version", success: false, data: []});
+            console.log(err);
+            res.status(404).send();
         } else if(result) {
             VersionModel.updateOne({database: database}, {$set: {version: version}}, (err, result) => {
                 if(err) {
-                    res.send({operation: "update", target: "version", success: false, data: []});
+                    console.log(err);
+                    res.status(404).send();
                 } else {
-                    res.send({operation: "update", target: "version", success: true, data: []});
+                    res.send(result);
                 }
             });
         } else {
             VersionModel.create({database: database, version: version}, (err, result) => {
                 if(err) {
-                    res.send({operation: "add", target: "version", success: false, data: []});
+                    console.log(err);
+                    res.status(404).send();
                 } else {
-                    res.send({operation: "add", target: "version", success: true, data: []});
+                    res.send(result);
                 }
             });
         }
@@ -189,19 +193,21 @@ app.delete('/location', (req, res) => {
     });
 });
 
-/* MARK: - Trajectory Model */
+// MARK: - Trajectory Model
 app.post('/trajectory', (req, res) => {
     console.log("POST /trajectory - " + Date())
     var points = req.body.points;
     var time = Date();
     TrajectoryModel.create({points: points, timestamp: time}, (err, result) => {
         if(err) {
-            res.send({operation: "add", target: "trajectory", success: false, data: []});
+            console.log(err);
+            res.status(404).send();
         } else {
-            res.send({operation: "add", target: "trajectory", success: true, data: []});
+            res.send({id: result._id, points: points, timestamp: time});
         }
     });
 });
+
 app.post('/trajectories', (req, res) => {
     console.log("POST /trajectories - " + Date())
     var trajectories = req.body.trajectories;
@@ -209,75 +215,74 @@ app.post('/trajectories', (req, res) => {
     var promises = []
     
     for(let i in trajectories) {
-        var promise = TrajectoryModel.create({points: trajectories[i], timestamp: time}, (err, result) => {
-            if(err) {
-                res.send({operation: "add", target: "trajectories", success: false, data: []});
-            }
+        var promise = new Promise(function(resolve, reject) {
+            TrajectoryModel.create({points: trajectories[i], timestamp: time}, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    res.status(404).send();
+                }
+                resolve({id: result._id, points: trajectories[i], timestamp: time});
+            });
         });
         promises.push(promise);
     }
     Promise.all(promises).then(value => {
-        res.send({operation: "add", target: "trajectories", success: true, data: []});
+        res.send(value);
     });
 });
 app.get('/trajectories', (req, res) => {
     console.log("GET /trajectories - " + Date());
-    TrajectoryModel.find({}, (err, result) => {
+    let aggr = [
+        { $match: {} },
+        { $project: { _id: 0, id: "$_id", points: "$points", timestamp: "$timestamp" } }
+    ];
+    TrajectoryModel.aggregate(aggr, (err, result) => {
         if(err) {
-            res.send({operation: "get", target: "trajectories", success: false, data: []});
-        } else {            
-            var trajs = []
-            for(var i in result) {
-                trajs.push(result[i].points)
-            }
-            res.send({operation: "get", target: "trajectories", success: true, data: trajs});
+            console.log(err);
+            res.status(404).send();
+        } else {
+            res.send(result);
         }
-    })
+    });
 });
 app.delete('/trajectory', (req, res) => {
     console.log("DELETE /trajectory - " + Date());
-    var ObjectId = require('mongodb').ObjectID
-    TrajectoryModel.deleteOne({_id: ObjectId(req.body.id)}, (err, result) => {
+    TrajectoryModel.deleteOne({_id: mongoose.Types.ObjectId(req.body.id)}, (err, result) => {
         if(err) {
-            res.send({operation: "delete", target: "trajectory", success: false, data: []});
+            console.log(err);
+            res.status(404).send();
         } else {
-            res.send({operation: "delete", target: "trajectory", success: true, data: []});
+            res.send(result);
         }
     });
 });
 
-app.post('/path', (req, res) => {
-    console.log("POST /path - " + Date());
-    var data = req.body.data;
-    LocationModel.findOne({name_en: data.start.name_en, type: data.start.type}, (err, result1) => {
-        if(result1) {
-            LocationModel.findOne({name_en: data.end.name_en, type: data.end.type}, (err, result2) => {
-                if(result2) {
-                    var conditions = {start: result1._id, end: result2._id, path: data.path, dist: data.dist, type: data.type}
-                    PathModel.create(conditions, (err, result) => {
-                        if(err) {
-                            res.send({operation: "add", target: "path", success: false, data: []});
-                        } else {
-                            res.send({operation: "add", target: "path", success: true, data: []});
-                        }
-                    });
-                } else {
-                    res.send({operation: "add", target: "path", success: false, data: []});
-                }
-            });
+// MARK: path between two location
+app.post('/route', (req, res) => {
+    console.log("POST /route - " + Date());
+    var conditions = {startId: req.body.startId, end: req.body.endId, points: req.body.points, dist: req.body.dist, type: req.body.type}
+    RouteModel.create(conditions, (err, result) => {
+        if(err) {
+            console.log(err);
+            res.status(404).send();
         } else {
-            res.send({operation: "add", target: "path", success: false, data: []});
+            res.send({id: result._id, startId: result.startId, endId: result.endId, points: points, dist: result.dist, type: result.type});
         }
     });
 });
 
-app.get('/paths', (req, res) => {
-    console.log("GET /paths - " + Date());
-    PathModel.find({}).populate([{path: 'start', select: 'name_en type -_id'}, {path: 'end', select: 'name_en type -_id'}]).exec((err, result) => {
+app.get('/routes', (req, res) => {
+    console.log("GET /routes - " + Date());
+    let aggr = [
+        { $match: {} },
+        { $project: { _id: 0, id: "$_id", startId: "$startId", endId: "$endId", points: "$points", dist: "$dist", type: "$type" } }
+    ];
+    RouteModel.aggregate(aggr, (err, result) => {
         if(err) {
-            res.send({operation: "get", target: "paths", success: false, data: []})
+            console.log(err);
+            res.status(404).send();
         } else {
-            res.send({operation: "get", target: "paths", success: true, data: result})
+            res.send(result);
         }
     });
 });
