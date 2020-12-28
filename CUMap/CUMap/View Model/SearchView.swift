59eaee6c6d9eb59a7e9e462b4493b53a526a0244
wanruuu <1155107790@link.cuhk.edu.hk>
2020-12-28 08:47:22 +0800

    adjust ui

--- CUMap/CUMap/View Model/SearchView.swift
+++ CUMap/CUMap/View Model/SearchView.swift
@@ -29,12 +29,24 @@
 import Foundation
 import SwiftUI
 
+let INF: Double = 99999
+
+struct DijDist {
+    var routes: [Route]
+    var dist: Double
+}
+
+enum TransMode {
+    case bus
+    case foot
+}
 struct SearchView: View {
     @Binding var locations: [Location]
     @Binding var routes: [Route]
     @Binding var plans: [[Route]]
     @ObservedObject var locationGetter: LocationGetterModel
 
+    @State var mode: TransMode = .bus
     @State var startName = ""
     @State var endName = ""
     @State var startId = ""
@@ -44,140 +56,57 @@ struct SearchView: View {
     
     var body: some View {
         if showStartList {
-            SearchList(locations: $locations, locationGetter: locationGetter, placeholder: "From", keyword: $startName, locationId: $startId, showList: $showStartList)
+            SearchList(locations: $locations, locationGetter: locationGetter, placeholder: "From", locationName: $startName, locationId: $startId, keyword: startName, showList: $showStartList)
         } else if showEndList {
-            SearchList(locations: $locations, locationGetter: locationGetter, placeholder: "To", keyword: $endName, locationId: $endId, showList: $showEndList)
+            SearchList(locations: $locations, locationGetter: locationGetter, placeholder: "To", locationName: $endName, locationId: $endId, keyword: endName, showList: $showEndList)
         } else {
             VStack {
-                SearchArea(showStartList: $showStartList, showEndList: $showEndList)
+                SearchArea(startName: startName, endName: endName, startId: startId, endId: endId, busTime: 0, footTime: -1, showStartList: $showStartList, showEndList: $showEndList, mode: $mode)
+                    .onAppear() {
+                        if startId != "" && endId != "" {
+                            dij()
+                        }
+                    }
                 Spacer()
             }
         }
     }
-    
-}
-/*
-struct SearchView: View {
-
-    var body: some View {
-        VStack {
-            // search input area
-            HStack {
-                VStack {
-                    // from
-                    HStack {
-                        showEndSearch ? nil : TextField("From", text: $start, onEditingChanged: { _ in })
-                            .onTapGesture {
-                                tmpKeyword = start
-                                tmpType = startType
-                                start = ""
-                                startType = -1
-                                showStartSearch = true
-                                showEndSearch = false
-                            }.textFieldStyle(RoundedBorderTextFieldStyle())
-                        showStartSearch ? Button(action: {
-                            showStartSearch = false
-                            start = tmpKeyword
-                            startType = tmpType
-                            hideKeyboard()
-                        }) { Text("Cancel")}.background(Color.white) : nil
-                    }
-                    // to
-                    HStack {
-                        showStartSearch ? nil : TextField("To", text: $end, onEditingChanged: { _ in })
-                            .onTapGesture {
-                                tmpKeyword = end
-                                tmpType = endType
-                                end = ""
-                                endType = -1
-                                showEndSearch = true
-                                showStartSearch = false
-                            }.textFieldStyle(RoundedBorderTextFieldStyle())
-                        showEndSearch ? Button(action: {
-                            showEndSearch = false
-                            end = tmpKeyword
-                            endType = tmpType
-                            hideKeyboard()
-                        }) { Text("Cancel")}.background(Color.white) : nil
-                    }
-                    
-                }
-                // search button
-                showStartSearch || showEndSearch ? nil : Button(action: {
-                    dij()
-                }) {Text("Search")}.background(Color.white)
-            }
-            // search list
-            showStartSearch ? List {
-                ForEach(0 ..< locations.count) { index in
-                    start == "" || locations[index].name_en.lowercased().contains(start.lowercased()) ?
-                        Button(action: {
-                            start = locations[index].name_en
-                            startType = locations[index].type
-                            showStartSearch = false
-                            hideKeyboard()
-                        }){ Text(locations[index].name_en) } : nil
-                }
-            } : nil
-            showEndSearch ? List {
-                ForEach(0 ..< locations.count) { index in
-                    end == "" || locations[index].name_en.lowercased().contains(end.lowercased()) ?
-                        Button(action: {
-                            end = locations[index].name_en
-                            endType = locations[index].type
-                            showEndSearch = false
-                            hideKeyboard()
-                        } ){ Text(locations[index].name_en) } : nil
-                }
-            } : nil
-            
-            Spacer()
-        }.padding()
-    }
     private func dij() {
         // Step 1: clean up result
-        result = []
+        plans = []
         
         // Step 2: initialize minDist & vertex set & queue
         let startIndex = indexOf(id: startId)
         let endIndex = indexOf(id: endId)
         
-        var minDist = [DijDist](repeating: DijDist(points: [], dist: INF), count: locations.count) // distance from start location to every location
+        var minDist = [DijDist](repeating: DijDist(routes: [], dist: INF), count: locations.count) // distance from start location to every location
         var checked = [Bool](repeating: false, count: locations.count)
         minDist[startIndex].dist = 0
         
         // Step 3: start
-        while(checked.filter{$0 == true}.count != checked.count) { // not all have been checked
+        while checked.filter({$0 == true}).count != checked.count { // not all have been checked
             // find the index of min dist who hasn't been checked
             var cur = -1
             var min = INF + 1.0
             for i in 0..<checked.count {
-                if(!checked[i] && minDist[i].dist < min) {
+                if !checked[i] && minDist[i].dist < min {
                     cur = i
                     min = minDist[i].dist
                 }
             }
             
             for route in routes {
-                if(route.startId == locations[cur].id) {
+                if route.startId == locations[cur].id {
                     let next = indexOf(id: route.endId)
-                    if(minDist[next].dist > minDist[cur].dist + route.dist) { // update
+                    if minDist[next].dist > minDist[cur].dist + route.dist { // update
                         minDist[next].dist = minDist[cur].dist + route.dist
-                        var newPoints = minDist[cur].points
-                        for i in 0..<route.points.count {
-                            newPoints.append(route.points[i])
-                        }
-                        minDist[next].points = newPoints
+                        minDist[next].routes = minDist[cur].routes + [route]
                     }
-                } else if(route.endId == locations[cur].id) {
+                } else if route.endId == locations[cur].id {
                     let next = indexOf(id: route.startId)
-                    if(minDist[next].dist > minDist[cur].dist + route.dist) { // update
+                    if minDist[next].dist > minDist[cur].dist + route.dist { // update
                         minDist[next].dist = minDist[cur].dist + route.dist
-                        var newPoints = minDist[cur].points
-                        for i in 0..<route.points.count {
-                            newPoints.append(route.points[route.points.count - 1 - i])
-                        }
-                        minDist[next].points = newPoints
+                        minDist[next].routes = minDist[cur].routes + [route]
                     }
                 }
             }
@@ -185,75 +114,95 @@ struct SearchView: View {
         }
         
         // Step 4: find the result
-        result = minDist[endIndex].points
+        plans.append(minDist[endIndex].routes)
     }
     private func indexOf(id: String) -> Int {
         for i in 0..<locations.count {
-            if(locations[i].id == id) {
+            if locations[i].id == id {
                 return i
             }
         }
         return -1
     }
+    
 }
-
-let INF: Double = 99999
-
-struct DijDist {
-    var points: [Coor3D]
-    var dist: Double
-}
-
-
-
-*/
 
 struct SearchArea: View {
+    @State var startName: String
+    @State var endName: String
+    @State var startId: String
+    @State var endId: String
+    
+    @State var busTime: Int
+    @State var footTime: Int
+    
     @Binding var showStartList: Bool
     @Binding var showEndList: Bool
+    @Binding var mode: TransMode
     
-    @State var startName = ""
-    @State var endName = ""
-    
-    @State var startId = ""
-    @State var endId = ""
-    
-    @State var showList = false
     var body: some View {
-        HStack {
-            VStack {
-                TextField("From", text: $startName)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray, lineWidth: 0.8))
-                    .onTapGesture {
-                        showStartList = true
-                    }
+        VStack(spacing: 15) {
+            HStack {
+                VStack {
+                    TextField("From", text: $startName)
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 0.8))
+                        .onTapGesture {
+                            showStartList = true
+                        }
 
-                TextField("To", text: $endName)
+                    TextField("To", text: $endName)
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 0.8))
+                        .onTapGesture {
+                            showEndList = true
+                        }
+                }
+                Image(systemName: "arrow.up.arrow.down")
+                    .imageScale(.large)
                     .padding()
-                    .background(Color.white)
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray, lineWidth: 0.8))
                     .onTapGesture {
-                        showEndList = true
+                        // swap
+                        var tmp = startName
+                        startName = endName
+                        endName = tmp
+                        tmp = startId
+                        startId = endId
+                        endId = tmp
                     }
             }
-            Image(systemName: "arrow.up.arrow.down")
-                .imageScale(.large)
-                .padding()
-                .onTapGesture {
-                    // swap
-                    var tmp = startName
-                    startName = endName
-                    endName = tmp
-                    tmp = startId
-                    startId = endId
-                    endId = tmp
+            ScrollView(.horizontal, showsIndicators: true) {
+                HStack(spacing: 30) {
+                    HStack {
+                        Image(systemName: "bus").foregroundColor(Color.black.opacity(0.7))
+                        busTime == -1 ? Text("—") : Text("\(busTime) min")
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 5)
+                    .background(mode == .bus ? CUPurple.opacity(0.2) : nil)
+                    .cornerRadius(20)
+                    .onTapGesture {
+                        mode = .bus
+                    }
+                    
+                    HStack {
+                        Image(systemName: "figure.walk").foregroundColor(Color.black.opacity(0.7))
+                        footTime == -1 ? Text("—") : Text("\(footTime) min")
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 5)
+                    .background(mode == .foot ? CUPurple.opacity(0.2) : nil)
+                    .cornerRadius(20)
+                    .onTapGesture {
+                        mode = .foot
+                    }
                 }
+            }
         }
         .padding()
+        .background(Color.white)
     }
 }
 
@@ -262,8 +211,10 @@ struct SearchList: View {
     @ObservedObject var locationGetter: LocationGetterModel
     var placeholder: String
     
-    @Binding var keyword: String
+    @Binding var locationName: String
     @Binding var locationId: String
+    
+    @State var keyword: String
     
     @Binding var showList: Bool
     
@@ -273,6 +224,9 @@ struct SearchList: View {
                 Image(systemName: "chevron.backward")
                     .imageScale(.large)
                     .padding(.trailing)
+                    .onTapGesture {
+                        showList = false
+                    }
                 TextField(placeholder, text: $keyword)
                 keyword == "" ? nil : Image(systemName: "xmark")
                     .imageScale(.large)
@@ -303,7 +257,7 @@ struct SearchList: View {
                 // other locations
                 ForEach(locations) { location in keyword == "" || location.name_en.lowercased().contains(keyword.lowercased()) ?
                     Button(action: {
-                        keyword = location.name_en
+                        locationName = location.name_en
                         locationId = location.id
                         showList = false
                     }) {
