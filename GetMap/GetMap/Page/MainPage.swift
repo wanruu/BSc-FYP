762df9d4c2ca8282e04059175b0a9024aba9e4c0
wanruuu <1155107790@link.cuhk.edu.hk>
2021-01-30 00:21:07 +0800

    simplify menu

--- GetMap/GetMap/Page/MainPage.swift
+++ GetMap/GetMap/Page/MainPage.swift
@@ -21,18 +21,15 @@ struct MainPage: View {
                     case .loc: LocationPage(current: $locationGetter.current)
                     case .bus: BusPage()
                 }
-                
-                MenuView(page: $page, showMenu: $showMenu)
-                    .offset(x: showMenu ? 0 : -UIScreen.main.bounds.width * 0.4)
-                    .animation(.easeIn)
             }
             .navigationBarTitle(Text(pageTitle()), displayMode: .inline)
-            .navigationBarItems(leading: Button(action: {
-                showMenu.toggle()
-            }) {
-                // Image(systemName: "list.bullet").imageScale(.large)
-                Text("Menu")
-            })
+            .navigationBarItems(leading:
+                Menu("Menu", content: {
+                    Button(action: { page = .traj }) { Text("Trajectory") }
+                    Button(action: { page = .loc }) { Text("Location") }
+                    Button(action: { page = .bus }) { Text("Bus") }
+                })
+            )
         }
     }
     
@@ -41,51 +38,6 @@ struct MainPage: View {
             case .traj: return "Trajectory"
             case .loc: return "Location"
             case .bus: return "Bus"
-        }
-    }
-}
-
-struct MenuView: View {
-    @Binding var page: Page
-    @Binding var showMenu: Bool
-    
-    var body: some View {
-        GeometryReader { geo in
-            VStack(alignment: .leading, spacing: 0) {
-                Divider()
-                
-                Button(action: {
-                    page = .traj
-                    showMenu.toggle()
-                }) {
-                    Text("Trajectory").frame(width: geo.size.width * 0.4)
-                }.buttonStyle(MyButtonStyle3(bgColor: Color.gray.opacity(0.5)))
-                
-                Divider()
-                
-                Button(action: {
-                    page = .loc
-                    showMenu.toggle()
-                }) {
-                    Text("Location").frame(width: geo.size.width * 0.4)
-                }.buttonStyle(MyButtonStyle3(bgColor: Color.gray.opacity(0.5)))
-                
-                Divider()
-                
-                Button(action: {
-                    page = .bus
-                    showMenu.toggle()
-                }) {
-                    Text("Bus").frame(width: geo.size.width * 0.4)
-                }.buttonStyle(MyButtonStyle3(bgColor: Color.gray.opacity(0.5)))
-                
-                Divider()
-            }
-            .frame(maxWidth: geo.size.width * 0.4, maxHeight: .infinity, alignment: .top)
-            .background(Color.white)
-            .clipped()
-            .shadow(radius: showMenu ? 10 : 0)
-            .ignoresSafeArea(.container, edges: .bottom)
         }
     }
 }
