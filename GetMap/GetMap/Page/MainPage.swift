3b866fb9be9aa01763a208a64de0c61b0df02352
wanruuu <1155107790@link.cuhk.edu.hk>
2021-02-06 00:30:42 +0800

    fix bug: Menu disappears when moving

--- GetMap/GetMap/Page/MainPage.swift
+++ GetMap/GetMap/Page/MainPage.swift
@@ -21,14 +21,21 @@ struct MainPage: View {
                     case .loc: LocationPage(current: $locationGetter.current)
                     case .bus: BusPage()
                 }
+                showMenu ? PageMenu(showing: $showMenu ,page: $page) : nil
+                
             }
             .navigationBarTitle(Text(pageTitle()), displayMode: .inline)
-            .navigationBarItems(leading:
-                Menu("Menu", content: {
+            .navigationBarItems(trailing:
+                Button(action: {
+                    showMenu.toggle()
+                }) {
+                    Text("Menu")
+                }
+                /*Menu("Menu") {
                     Button(action: { page = .traj }) { Text("Trajectory") }
                     Button(action: { page = .loc }) { Text("Location") }
                     Button(action: { page = .bus }) { Text("Bus") }
-                })
+                }*/
             )
         }
     }
@@ -38,6 +45,53 @@ struct MainPage: View {
             case .traj: return "Trajectory"
             case .loc: return "Location"
             case .bus: return "Bus"
+        }
+    }
+}
+
+struct PageMenu: View {
+    @Binding var showing: Bool
+    @Binding var page: Page
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.2)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea(.all)
+                .onTapGesture {
+                    showing.toggle()
+                }
+
+            VStack(spacing: 0) {
+                Button(action: {
+                    page = .traj
+                    showing.toggle()
+                }) {
+                    Text("Trajectory").padding().frame(maxWidth: .infinity)
+                }.buttonStyle(MyButtonStyle3(bgColor: Color.gray.opacity(0.5)))
+                
+                Divider()
+                
+                Button(action: {
+                    page = .loc
+                    showing.toggle()
+                }) {
+                    Text("Location").padding().frame(maxWidth: .infinity)
+                }.buttonStyle(MyButtonStyle3(bgColor: Color.gray.opacity(0.5)))
+                
+                Divider()
+                
+                Button(action: {
+                    page = .bus
+                    showing.toggle()
+                }) {
+                    Text("Bus").padding().frame(maxWidth: .infinity)
+                }.buttonStyle(MyButtonStyle3(bgColor: Color.gray.opacity(0.5)))
+            }
+            .background(Color.white)
+            .cornerRadius(10)
+            .clipped()
+            .shadow(radius: 10)
+            .padding(.horizontal)
         }
     }
 }
