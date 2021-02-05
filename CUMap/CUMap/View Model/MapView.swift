005cd44bce7e0f9afd31e446ea5b1bbee145a21d
wanruuu <1155107790@link.cuhk.edu.hk>
2021-02-06 03:36:52 +0800

    improve display of plan

--- CUMap/CUMap/View Model/MapView.swift
+++ CUMap/CUMap/View Model/MapView.swift
@@ -59,16 +59,7 @@ struct MapView: View {
                 .frame(width: 3200 * scale, height: 3200 * 25 / 20 * scale, alignment: .center)
                 .position(x: UIScreen.main.bounds.width / 2 + offset.x, y: UIScreen.main.bounds.height / 2 + offset.y)
             
-            // TODO: show plans in map
-            /*ForEach(plans) { plan in
-                PlanMapView(plan: plan, opacity: 0, offset: $offset, scale: $scale)
-                    .onTapGesture {
-                        print("switch plan")
-                        chosenPlan = plan
-                    }
-                
-            }*/
-            chosenPlan != nil ? PlanMapView(plan: chosenPlan!, opacity: 1, offset: $offset, scale: $scale) : nil
+            chosenPlan != nil ? PlanMapView(plan: $chosenPlan, opacity: 1, offset: $offset, scale: $scale) : nil
             
             // current location
             UserPoint(locationGetter: locationGetter, offset: $offset, scale: $scale)
@@ -97,33 +88,35 @@ extension DrawPoint: Identifiable {
 
 // draw a plan
 struct PlanMapView: View {
-    @State var plan: Plan
+    @Binding var plan: Plan?
     @State var opacity: Double
     @Binding var offset: Offset
     @Binding var scale: CGFloat
     
     var body: some View {
         // calculate points list to draw, starting point and ending point may not included
-        let DIST: CGFloat = (innerRadius * 1.5) * maxZoomIn / scale // distance between two point
+        let DIST: CGFloat = (innerRadius * 1.5) * maxZoomIn / scale // min distance between two point
         var points: [DrawPoint] = []
         
-        points.append(DrawPoint(type: plan.routes[0].type, x: CGFloat((plan.routes[0].points[0].longitude - centerLg) * lgScale * 2), y: CGFloat((centerLa - plan.routes[0].points[0].latitude) * laScale * 2)))
-        
-        var distSoFar: CGFloat = 0
-        
-        for route in plan.routes {
-            for point in route.points {
-                let lastPoint = points.last!
-                let thisPoint = DrawPoint(type: route.type, x: CGFloat((point.longitude - centerLg) * lgScale * 2), y: CGFloat((centerLa - point.latitude) * laScale * 2))
-                
-                let dist = pow((lastPoint.x - thisPoint.x) * (lastPoint.x - thisPoint.x) + (lastPoint.y - thisPoint.y) * (lastPoint.y - thisPoint.y), 0.5)
-                
-                if distSoFar + dist < DIST {
-                    distSoFar += dist
-                } else {
-                    distSoFar = 0
-                    // TODO: change color of point by route type
-                    points.append(thisPoint)
+        if plan != nil {
+            points.append(DrawPoint(type: plan!.routes[0].type, x: CGFloat((plan!.routes[0].points[0].longitude - centerLg) * lgScale * 2), y: CGFloat((centerLa - plan!.routes[0].points[0].latitude) * laScale * 2)))
+            
+            var distSoFar: CGFloat = 0
+            
+            for route in plan!.routes {
+                for point in route.points {
+                    let lastPoint = points.last!
+                    let thisPoint = DrawPoint(type: route.type, x: CGFloat((point.longitude - centerLg) * lgScale * 2), y: CGFloat((centerLa - point.latitude) * laScale * 2))
+                    
+                    let dist = pow((lastPoint.x - thisPoint.x) * (lastPoint.x - thisPoint.x) + (lastPoint.y - thisPoint.y) * (lastPoint.y - thisPoint.y), 0.5)
+                    
+                    if distSoFar + dist < DIST {
+                        distSoFar += dist
+                    } else {
+                        distSoFar = 0
+                        // TODO: change color of point by route type
+                        points.append(thisPoint)
+                    }
                 }
             }
         }
@@ -133,6 +126,7 @@ struct PlanMapView: View {
                 Circle()
                     .frame(width: innerRadius, height: innerRadius, alignment: .center)
                     .foregroundColor(point.type == 0 ? CUPurple : CUYellow)
+                    .overlay(Circle().stroke(Color.black).frame(width: innerRadius+1, height: innerRadius+1, alignment: .center))
                     .position(x: UIScreen.main.bounds.width / 2 + point.x * scale + offset.x, y: UIScreen.main.bounds.height / 2 + point.y * scale + offset.y)
             }
     }
