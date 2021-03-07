41c2816ebf7d0eb6a931fc4f52f200c65dd9346a
wanruuu <1155107790@link.cuhk.edu.hk>
2021-03-08 04:43:57 +0800

    improve map display

--- CU Map/CU Map/Pages/NaviPage/NaviMapView.swift
+++ CU Map/CU Map/Pages/NaviPage/NaviMapView.swift
@@ -1,14 +1,12 @@
 import SwiftUI
 import MapKit
 
-var lineColor: Color = CU_YELLOW
+// var lineColor: Color = CU_YELLOW
 
 struct NaviMapView: UIViewRepresentable {
     @Binding var startLoc: Location?
     @Binding var endLoc: Location?
     @Binding var selectedPlan: Plan?
-    
-    @State var trackingMode: MKUserTrackingMode = .follow
     
     // annotation
     @State var startAnt = MKPointAnnotation()
@@ -18,50 +16,84 @@ struct NaviMapView: UIViewRepresentable {
         let mapView = MKMapView()
         mapView.delegate = context.coordinator
         mapView.showsUserLocation = true
+        mapView.addAnnotation(startAnt)
+        mapView.addAnnotation(endAnt)
         return mapView
     }
     
     func updateUIView (_ mapView: MKMapView, context: Context) {
-        // update start/end annotation
-        if startLoc != nil {
-            startAnt.title = "Start"
-            startAnt.subtitle = startLoc!.nameEn
-            startAnt.coordinate = CLLocationCoordinate2D(latitude: startLoc!.latitude, longitude: startLoc!.longitude)
-            mapView.addAnnotation(startAnt)
-        } else {
-            mapView.removeAnnotation(startAnt)
+        if let startLoc = startLoc {
+            startAnt.title = startLoc.nameEn
+            startAnt.coordinate = CLLocationCoordinate2D(latitude: startLoc.latitude, longitude: startLoc.longitude)
         }
-        if endLoc != nil {
-            endAnt.title = "End"
-            endAnt.subtitle = endLoc!.nameEn
-            endAnt.coordinate = CLLocationCoordinate2D(latitude: endLoc!.latitude, longitude: endLoc!.longitude)
-            mapView.addAnnotation(endAnt)
-        } else {
-            mapView.removeAnnotation(endAnt)
+        if let endLoc = endLoc {
+            endAnt.title = endLoc.nameEn
+            endAnt.coordinate = CLLocationCoordinate2D(latitude: endLoc.latitude, longitude: endLoc.longitude)
+            mapView.setRegion(MKCoordinateRegion(center: endAnt.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
         }
         
         // update plan annotation
         mapView.removeOverlays(mapView.overlays)
-        if selectedPlan != nil {
+        if let plan = selectedPlan {
+            // by bus
             var busPolylines: [MKPolyline] = []
-            var walkPolylines: [MKPolyline] = []
-            for route in selectedPlan!.routes {
-                var points: [CLLocationCoordinate2D] = []
+            for route in plan.routes.filter({ $0.type == .byBus }) {
+                let points = route.points.map({ CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) })
+                busPolylines.append(MKPolyline(coordinates: points, count: points.count))
+            }
+            mapView.addOverlay(MKMultiPolyline(busPolylines))
+            
+            // on foot
+            for route in plan.routes.filter({ $0.type == .onFoot}) {
+                mapView.addOverlays(route.points.map({ MKCircle(center: CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude), radius: 8) }))
+            }
+            
+            // set region
+            var minLat: Double = .infinity
+            var maxLat: Double = -.infinity
+            var minLng: Double = .infinity
+            var maxLng: Double = -.infinity
+            for route in plan.routes {
                 for point in route.points {
-                    points.append(CLLocationCoordinate2D(latitude: point.latitude, longitude: point.longitude))
-                }
-                if route.type == .onFoot { // walk
-                    walkPolylines.append(MKPolyline(coordinates: points, count: points.count))
-                } else if route.type == .byBus { // bus
-                    busPolylines.append(MKPolyline(coordinates: points, count: points.count))
+                    minLat = min(minLat, point.latitude)
+                    maxLat = max(maxLat, point.latitude)
+                    minLng = min(minLng, point.longitude)
+                    maxLng = max(maxLng, point.longitude)
                 }
             }
-            lineColor = CU_PURPLE
-            mapView.addOverlay(MKMultiPolyline(busPolylines))
-            lineColor = CU_YELLOW
-            mapView.addOverlay(MKMultiPolyline(walkPolylines))
-            
-            mapView.setRegion(CENTER_REGION, animated: true)
+            mapView.setRegion(
+                MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(latitude: (minLat + maxLat) / 2, longitude: (minLng + maxLng) / 2),
+                    span: MKCoordinateSpan(latitudeDelta: (maxLat - minLat) * 1.5, longitudeDelta: (maxLng - minLng) * 1.5)
+                ),
+                animated: true
+            )
+        } else {
+            if let startLoc = startLoc, let endLoc = endLoc {
+                mapView.setRegion(
+                    MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: (startLoc.latitude + endLoc.latitude) / 2, longitude: (startLoc.longitude + endLoc.longitude) / 2),
+                        span: MKCoordinateSpan(latitudeDelta: abs(startLoc.latitude - endLoc.latitude) * 1.5, longitudeDelta: abs(startLoc.longitude - endLoc.longitude) * 1.5)
+                    ),
+                    animated: true
+                )
+            } else if let startLoc = startLoc {
+                mapView.setRegion(
+                    MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: startLoc.latitude, longitude: startLoc.longitude),
+                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                    ),
+                    animated: true
+                )
+            } else if let endLoc = endLoc {
+                mapView.setRegion(
+                    MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: endLoc.latitude, longitude: endLoc.longitude),
+                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                    ),
+                    animated: true
+                )
+            }
         }
     }
     
@@ -81,15 +113,31 @@ struct NaviMapView: UIViewRepresentable {
         func mapView (_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
             if overlay is MKMultiPolyline {
                 let renderer = MKMultiPolylineRenderer(multiPolyline: overlay as! MKMultiPolyline)
-                renderer.strokeColor = UIColor(lineColor)
+                renderer.strokeColor = UIColor(CU_YELLOW.opacity(0.6))
                 renderer.lineWidth = 3
+                return renderer
+            } else if overlay is MKCircle {
+                let renderer = MKCircleRenderer(circle: overlay as! MKCircle)
+                renderer.fillColor = UIColor(CU_PURPLE)
                 return renderer
             }
             return MKOverlayRenderer()
         }
         
-        func mapViewDidFinishLoadingMap (_ mapView: MKMapView) {
-            mapView.setUserTrackingMode(.follow, animated: true)
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            if annotation as? MKPointAnnotation == parent.startAnt {
+                let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "startLoc")
+                annotationView.glyphText = "Start"
+                return annotationView
+            } else if annotation as? MKPointAnnotation == parent.endAnt {
+                let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "endLoc")
+                annotationView.glyphText = "End"
+                return annotationView
+            } else if annotation is MKUserLocation {
+                let annotationView = MKUserLocationView(annotation: annotation, reuseIdentifier: "user")
+                return annotationView
+            }
+            return nil
         }
     }
 }
