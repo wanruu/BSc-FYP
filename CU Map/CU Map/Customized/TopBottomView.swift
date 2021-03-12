f332ddc0db9f6801debf3f2e0f5cd54a1e840ad6
wanruuu <1155107790@link.cuhk.edu.hk>
2021-03-13 02:42:39 +0800

    improve navipage

--- CU Map/CU Map/Customized/TopBottomView.swift
+++ CU Map/CU Map/Customized/TopBottomView.swift
@@ -1,30 +1,20 @@
 import SwiftUI
 
-enum BottomSheetHeight {
-    case large, medium, small
-    
-    func toCGFloat() -> CGFloat {
-        switch self {
-        case .large: return UIScreen.main.bounds.height * 0.9
-        case .medium: return UIScreen.main.bounds.height * 0.5
-        case .small: return UIScreen.main.bounds.height * 0.2
-        }
-    }
-}
-
-struct BottomSheetView<Content: View>: View {
+struct TopBottomView<Top: View, Bottom: View>: View {
     @Environment(\.colorScheme) var colorScheme
     @State var curHeight: BottomSheetHeight = .small
     let largeHeight = BottomSheetHeight.large.toCGFloat()
     let mediumHeight = BottomSheetHeight.medium.toCGFloat()
     let smallHeight = BottomSheetHeight.small.toCGFloat()
     
-    @Binding var showing: Bool
-    let content: Content
+    @Binding var showBottom: Bool
+    let top: Top
+    let bottom: Bottom
     
-    init(showing: Binding<Bool>, @ViewBuilder content: () -> Content) {
-        self.content = content()
-        self._showing = showing
+    init(showBottom: Binding<Bool>, @ViewBuilder top: () -> Top, @ViewBuilder bottom: () -> Bottom) {
+        self.top = top()
+        self.bottom = bottom()
+        self._showBottom = showBottom
     }
     
     // gesture
@@ -67,42 +57,64 @@ struct BottomSheetView<Content: View>: View {
             }
     }
     
-    // indicator
-    var indicator: some View {
-        RoundedRectangle(cornerRadius: 10)
-            .fill(Color.secondary)
-            .frame(width: 40, height: 3)
-            .padding()
-    }
-    
-    var offset: CGFloat {
-        if showing {
+    // bottom offset
+    var bottomOffset: CGFloat {
+        if showBottom {
             return largeHeight - curHeight.toCGFloat() + translation
         } else {
             return largeHeight
         }
     }
     
+    var topOffset: CGFloat {
+        if !showBottom || curHeight == .small {
+            return 0
+        } else {
+            return -curHeight.toCGFloat()
+        }
+    }
+    
+    // indicator
+    var indicator: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.secondary)
+            .frame(width: 40, height: 3)
+            .padding(20)
+    }
+    
     // body
     var body: some View {
         let bgColor = colorScheme == .dark ? Color.black : Color.white
-        GeometryReader { geometry in
-            VStack {
-                Spacer()
-                VStack(spacing: 0) {
-                    indicator
-                    content
-                }
-                .frame(width: geometry.size.width, height: largeHeight, alignment: .top)
-                .background(bgColor)
-                .cornerRadius(10)
-                .offset(y: offset)
-                .animation(.interactiveSpring(), value: showing)
-                .animation(.interactiveSpring(), value: translation)
-                .shadow(radius: 5)
+        ZStack {
+            GeometryReader { geometry in
+                top
+                    .frame(width: geometry.size.width)
+                    .padding(.top, geometry.safeAreaInsets.top)
+                    .background(bgColor)
+                    .clipped()
+                    .shadow(radius: 5)
+                    .offset(y: -geometry.safeAreaInsets.top + topOffset)
+                    .animation(Animation.easeIn(duration: 0.5), value: translation)
             }
+            GeometryReader { geometry in
+                VStack {
+                    Spacer()
+                    VStack(spacing: 0) {
+                        indicator
+                        bottom
+                            .frame(height: curHeight.toCGFloat() - 43)
+                    }
+                    .frame(width: geometry.size.width, height: largeHeight, alignment: .top)
+                    .background(bgColor)
+                    .cornerRadius(10)
+                    .offset(y: bottomOffset)
+                    .animation(.interactiveSpring(), value: showBottom)
+                    .animation(.interactiveSpring(), value: translation)
+                    .shadow(radius: 5)
+                }
+            }
+            .gesture(drag)
+            .edgesIgnoringSafeArea(.bottom)
         }
-        .gesture(drag)
-        .edgesIgnoringSafeArea(.all)
     }
 }
